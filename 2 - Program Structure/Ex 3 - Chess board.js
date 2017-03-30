fd3257776b039c9445a1af450a9df5dc121a7661
Martin Kaldramov <martinkaldramov1@yahoo.com>
2017-03-30 23:21:30 +0300

    Exercise 3 solution

---
+++ 2 - Program Structure/Ex 3 - Chess board.js
@@ -0,0 +1,34 @@
+/*
+
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines.
+At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
+
+Passing this string to console.log should show something like this:
+
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+
+When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size,
+outputting a grid of the given width and height.
+
+*/
+
+var chessBoard = "";
+
+for (var x = 0; x < 8; x++) {
+  for (var y = 0; y < 8; y++){
+    if ((x + y) % 2 == 0)
+      chessBoard += " ";
+    else
+      chessBoard += "#";
+  }
+  chessBoard += "\n";
+}
+
+console.log(chessBoard);
